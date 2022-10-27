c8b1ddab13818f44fa5e1699a39870b74043f25e
JasonAMBA <ambajason2@gmail.com>
2022-10-27 08:13:30 +0100

    feature_api_type_salle controller and service

--- api/src/main/java/fr/imie/fomation/api/controller/TypeSalleController.java
+++ api/src/main/java/fr/imie/fomation/api/controller/TypeSalleController.java
@@ -2,8 +2,13 @@ package fr.imie.fomation.api.controller;
 
 import fr.imie.fomation.api.model.TypeSalle;
 import fr.imie.fomation.api.service.TypeSalleService;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +17,11 @@ public class TypeSalleController {
 
     @Autowired
     private TypeSalleService typeSalleService;
+    
+    @GetMapping("/type-salles")
+    public Iterable<TypeSalle> getTypeSalles() {
+        return typeSalleService.getTypeSalles();
+    }
 
     /**
      * Create - new type salle
@@ -21,6 +31,38 @@ public class TypeSalleController {
     @PostMapping("/add-type-salle")
     public TypeSalle createTypeSalle(@RequestBody TypeSalle typeSalle) {
       return typeSalleService.saveTypeSalle(typeSalle);
+    }
+    
+    @GetMapping("/type-salle/{id}")
+    public TypeSalle getTypeSalle (@PathVariable("id") final Long id) {
+        Optional<TypeSalle> typesalle = typeSalleService.getTypeSalle(id);
+        if(typesalle.isPresent()) {
+            return typesalle.get();
+        } else {
+            return null;
+        }
+    }
+    
+    @PutMapping("/type-salle/{id}")
+    public TypeSalle updateTypeSalle(@PathVariable("id") final Long id, @RequestBody TypeSalle typesalle) {
+        Optional<TypeSalle> t = typeSalleService.getTypeSalle(id);
+        if(t.isPresent()) {
+            TypeSalle currentTypeSalle = t.get();
+            
+            String nom = typesalle.getNom();
+            if(nom != null) {
+                currentTypeSalle.setNom(nom);
+            }
+            typeSalleService.saveTypeSalle(currentTypeSalle);
+            return currentTypeSalle;
+        } else {
+            return null;
+        }
+    }
+    
+    @DeleteMapping("/type-salle/{id}")
+    public void deleteTypeSalle(@PathVariable("id") final Long id) {
+        typeSalleService.deleteTypeSalle(id);
     }
 
 }
--- api/src/main/java/fr/imie/fomation/api/service/TypeSalleService.java
+++ api/src/main/java/fr/imie/fomation/api/service/TypeSalleService.java
@@ -2,6 +2,7 @@ package fr.imie.fomation.api.service;
 
 import fr.imie.fomation.api.model.TypeSalle;
 import fr.imie.fomation.api.repository.TypeSalleRepository;
+import java.util.Optional;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,18 @@ public class TypeSalleService {
 
     @Autowired
     private TypeSalleRepository typeSalleRepository;
+    
+    public Optional<TypeSalle> getTypeSalle(final Long id) {
+        return typeSalleRepository.findById(id);
+    }
+    
+    public Iterable<TypeSalle> getTypeSalles() {
+        return typeSalleRepository.findAll();
+    }
+    
+    public void deleteTypeSalle(final Long id) {
+        typeSalleRepository.deleteById(id);
+    }
 
     /**
      * INSERT - Insert type salle
