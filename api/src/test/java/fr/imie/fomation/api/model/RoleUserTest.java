83f25002cb217dc1b30136b2a03faefa1b275809
nicolas zanado <nicolas_zanardo@live.fr>
2022-11-08 11:57:33 +0100

    feature_role update

--- api/src/test/java/fr/imie/fomation/api/model/RoleUserTest.java
+++ api/src/test/java/fr/imie/fomation/api/model/RoleUserTest.java
@@ -1,14 +1,11 @@
 package fr.imie.fomation.api.model;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Nicolas Zanardo
